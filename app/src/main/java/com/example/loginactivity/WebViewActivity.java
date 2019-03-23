3df0c39addd51483af89d71e2980b5a0119b8118
GOKHAN KARA <GOKHANK@toyota.com.tr>
2019-03-23 09:31:05 +0300

    WebView Upgrade

--- app/src/main/java/com/example/loginactivity/WebViewActivity.java
+++ app/src/main/java/com/example/loginactivity/WebViewActivity.java
@@ -1,8 +1,11 @@
 package com.example.loginactivity;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -12,8 +15,26 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
 
-        WebView webview = (WebView) findViewById(R.id.tarayici);
-       // webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("https://gelecegiyazanlar.turkcell.com.tr");
+        WebView webView = (WebView) findViewById(R.id.tarayici);
+       // webView.getSettings().setJavaScriptEnabled(true);
+
+
+        // Sayfanın yüklendiğinin anlaşılması için ProgressDialog açıyoruz.
+        final ProgressDialog progressDialog = ProgressDialog.show(this, "Asistan'ım",
+                "Sayfa Yükleniyor...", true);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            // Sayfanın yüklenme işlemi bittiğinde progressDialog'u kapatıyoruz.
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                if (progressDialog.isShowing())
+                    progressDialog.dismiss();
+            }
+        });
+
+        webView.loadUrl("https://gelecegiyazanlar.turkcell.com.tr");
+
     }
 }
