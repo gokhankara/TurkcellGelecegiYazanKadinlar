40ee36da4e4950d671a7dc2fbfec04e0f56b25c4
GOKHAN KARA <GOKHANK@toyota.com.tr>
2019-03-24 16:20:22 +0300

    OK

--- app/src/main/java/com/example/loginactivity/FotoVeVideo.java
+++ app/src/main/java/com/example/loginactivity/FotoVeVideo.java
@@ -16,6 +16,8 @@ public class FotoVeVideo extends AppCompatActivity {
     Button btnFotoCek;
     Button btnVideoCek;
 
+    Button fotoCek;
+
     private static final int VIDEO_ACTION_CODE = 101;
     private static final int IMAGE_ACTION_CODE = 102;
 
@@ -24,46 +26,47 @@ public class FotoVeVideo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foto_ve_video);
 
-        btnFotoCek = (Button) findViewById(R.id.btnFotoCek);
-        btnVideoCek = (Button) findViewById(R.id.btnVideoCek);
+        fotoCek = (Button) findViewById(R.id.btnVideoCek);
+        videoKaydet = (Button) findViewById(R.id.btnFotoCek);
 
-        btnFotoCek.setOnClickListener(new View.OnClickListener() {
+
+        fotoCek.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View viw){
-                Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(fotoIntent);
+            public void onClick(View view) {
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePhotoIntent, IMAGE_ACTION_CODE);
             }
         });
 
-        btnVideoCek.setOnClickListener(new View.OnClickListener() {
+        videoKaydet.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View viw){
-                Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-                startActivity(videoIntent);
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(takePictureIntent, VIDEO_ACTION_CODE);
             }
         });
     }
 
     @Override
-    public void onActivityResult(int istekKodu, int sonucKodu, Intent data){
-        super.onActivityResult(istekKodu,sonucKodu,data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        if (sonucKodu != RESULT_OK) return;
+        if(resultCode != RESULT_OK) return;
 
-        switch (istekKodu){
-
-            case VIDEO_ACTION_CODE:
-                VideoView videoView = (VideoView) findViewById(R.id.videoViewOnizleme);
+        switch (requestCode) {
+            case VIDEO_ACTION_CODE :
+                VideoView videoView = ((VideoView) findViewById(R.id.videoViewOnizleme));
                 videoView.setVideoURI(data.getData());
                 videoView.setMediaController(new MediaController(this));
                 videoView.requestFocus();
                 videoView.start();
-            break;
-            case IMAGE_ACTION_CODE:
+                break;
+
+            case IMAGE_ACTION_CODE :
                 Bundle extras = data.getExtras();
-                ((ImageView)findViewById(R.id.imgViewOnizleme)).setImageBitmap((Bitmap) extras.get("data"));
-            break;
-            default :
+                ((ImageView) findViewById(R.id.imgViewOnizleme)).setImageBitmap((Bitmap) extras.get("data"));
+                break;
+            default:
                 break;
         }
 
