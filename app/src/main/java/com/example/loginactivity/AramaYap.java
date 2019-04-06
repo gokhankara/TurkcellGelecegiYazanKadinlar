39e28e1373fef33bc19a4519dcc88ef7dde7db2f
GOKHAN KARA <GOKHANK@toyota.com.tr>
2019-04-06 11:35:08 +0300

    Arama Activity tamamlandı

--- app/src/main/java/com/example/loginactivity/AramaYap.java
+++ app/src/main/java/com/example/loginactivity/AramaYap.java
@@ -1,7 +1,12 @@
 package com.example.loginactivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AramaYap extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class AramaYap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arama_yap);
+
+        final EditText telNo = (EditText) findViewById(R.id.txtTelNo);
+        Button ara = (Button) findViewById(R.id.btnAra);
+
+        ara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+             String numara = telNo.getText().toString();
+             Intent aramaInten = new Intent(Intent.ACTION_DIAL);
+
+            aramaInten.setData(Uri.parse("tel:"+numara));
+            startActivity(aramaInten);
+
+            }
+        });
+
     }
 }
