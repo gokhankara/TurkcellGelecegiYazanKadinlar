5d89c415c5b917e6efc36a5bf532335b3d943ec5
GOKHAN KARA <GOKHANK@toyota.com.tr>
2019-04-06 13:05:36 +0300

    SesKayit İslemleri 1.Asama

--- app/src/main/java/com/example/loginactivity/SesKayit.java
+++ app/src/main/java/com/example/loginactivity/SesKayit.java
@@ -1,13 +1,65 @@
 package com.example.loginactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SesKayit extends AppCompatActivity {
+public class SesKayit extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnKaydet;
+    Button btnDurdur;
+    Button btnCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ses_kayit);
+
+        btnKaydet = (Button) findViewById(R.id.btnKaydet);
+        btnKaydet.setOnClickListener((View.OnClickListener) this);
+
+        btnDurdur = (Button) findViewById(R.id.btnDurdur);
+        btnDurdur.setOnClickListener((View.OnClickListener) this);
+
+        btnCal = (Button) findViewById(R.id.btnCal);
+        btnCal.setOnClickListener((View.OnClickListener) this);
+
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnKaydet) {
+            Toast.makeText(getApplicationContext(), btnKaydet.getText(), Toast.LENGTH_LONG).show();
+            //Intent intent = new Intent(Menu.this, map.class);
+            // startActivity(intent);
+            SesKaydet();
+        }
+        else if (view == btnDurdur) {
+            Toast.makeText(getApplicationContext(), btnDurdur.getText(), Toast.LENGTH_LONG).show();
+            //Intent intent = new Intent(Menu.this, map.class);
+            // startActivity(intent);
+            SesDurdur();
+        }
+        else if (view == btnCal) {
+            Toast.makeText(getApplicationContext(), btnCal.getText(), Toast.LENGTH_LONG).show();
+            //Intent intent = new Intent(Menu.this, map.class);
+            // startActivity(intent);
+            SesCal();
+        }
+    }
+
+    private void SesCal() {
+    }
+
+    private void SesDurdur() {
+    }
+
+    private void SesKaydet() {
+
+    }
+
+
 }
