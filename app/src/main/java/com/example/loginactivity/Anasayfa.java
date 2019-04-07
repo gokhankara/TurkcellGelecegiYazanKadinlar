efc66dbfa75473c12709e4b50f700e2322ce5435
GOKHAN KARA <GOKHANK@toyota.com.tr>
2019-04-07 11:38:10 +0300

    NavigationBar Eklendi

---
+++ app/src/main/java/com/example/loginactivity/Anasayfa.java
@@ -0,0 +1,13 @@
+package com.example.loginactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Anasayfa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anasayfa);
+    }
+}
